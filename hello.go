adc77ac4682cf1890c13758a80be85a1266602f8
idonkeyliu <232836108@qq.com>
2016-11-09 18:13:30 +0800

    update

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main{
+	/* this is my first go demo */
+	fmt.PrintLn("Hello, world")
+
+	var teerCount int
+	teerCount = 10
+	fmt.PrintLn(Hello,world", teerCount)
+}
